e76e348f878183eabdde89b321bcfe461c055eba
oguri-amedo <mungcal.d.lewis@gmail.com>
2022-09-26 15:37:01 +0900

    チームメンバーの情報を変数に格納

--- loop.rb
+++ loop.rb
@@ -1,12 +1,13 @@
 # 繰り返し処理。Rubyで主に使われるのは each
 # 配列を作成
+#
 
-users = ["saitou","tanaka","yamada","shimizu","yanagi","obata"]
+user_list = ["saitou","tanaka","yamada","shimizu","yanagi","obata"]
 
 team_a = []
 team_b = []
 
-users.each.with_index do |user,i|
+user_list.each.with_index do |user,i|
   # 奇数：true 偶数：false
   # even? : 偶数:true 奇数:false
   if i.odd?
@@ -17,11 +18,28 @@ users.each.with_index do |user,i|
   end
 end
 
-  puts "チームA"
-  puts team_a
-  puts ""
-  puts "チームB"
-  puts team_b
+# チームAのメンバー表示
+text = "チームA"
+team_a.each do |member|
+  # テキストに文字列連結
+  text += member
+end
+# チームBのメンバーを表示
+text += "チームB"
+team_b.each do |member|
+  text += member
+end
+
+# チーム分けの情報を表示  
+puts text
+
+
+  # # テキストに文字列連結
+  # puts "チームA"
+  # puts team_a
+  # puts ""
+  # puts "チームB"
+  # puts team_b
 
 # # 繰り返し処理 配列の値が以下のuserに代入されて、出力した際に表示される
 # users.each do |user|
